889d26670fc1f2df86035ceb8df61a7d4c83d9d3
JaiRaga <raga.j.santhosh@gmail.com>
2021-11-13 21:26:52 +0530

    :art: styles for Home Screen

---
+++ screens/HomeScreen/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 10,
+		marginTop: 10,
+		// backgroundColor: '#fff',
+	},
+	heading: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	image: {
+		width: 110,
+		height: 200,
+		resizeMode: 'cover',
+		borderRadius: 5,
+		margin: 5,
+	},
+})
+
+export default styles
